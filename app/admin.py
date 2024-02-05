8c6b08cae2107e0befa65d316ae4174a9e75e4ab
behruz171 <behruzzo662@gmail.com>
2024-02-05 20:23:35 +0500

    first commit

---
+++ app/admin.py
@@ -0,0 +1,40 @@
+from django.contrib import admin
+from .models import *
+
+@admin.register(Profile)
+class Profile(admin.ModelAdmin):
+    list_display = ('full_name',)
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+@admin.register(About)
+class AboutAdmin(admin.ModelAdmin):
+    list_display = ('experience',)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('author', 'id',)
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+@admin.register(Portfolio)
+class PortfolioAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+# @admin.site.register(Skill)
+# @admin.site.register(About)
+# @admin.site.register(Category)
+# @admin.site.register(Post)
+# @admin.site.register(Service)
+# @admin.site.register(Portfolio)
+# class MemberAdmin(admin.ModelAdmin):
+#     ...
+
+
---
+++ block/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from app.views import index, about, portfolio, blog, blog_detail
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',index),
+    path("about/", about),
+    path('portfolio/', portfolio),
+    path('blog/', blog, name='blog'),
+    path('blog/<int:pk>/', blog_detail, name='blog_detail'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
